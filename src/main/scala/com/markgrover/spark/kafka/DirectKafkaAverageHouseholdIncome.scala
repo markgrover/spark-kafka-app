ad11d5b92e4277ec9f498dc24913a4272cf64565
Mark Grover <mark@apache.org>
2016-01-06 14:00:43 -0800

    More changes

--- src/main/scala/com/markgrover/spark/kafka/DirectKafkaAverageHouseholdIncome.scala
+++ src/main/scala/com/markgrover/spark/kafka/DirectKafkaAverageHouseholdIncome.scala
@@ -6,18 +6,15 @@ object DirectKafkaAverageHouseholdIncome {
     import org.apache.spark.SparkConf
     import org.apache.spark.streaming._
 
-    val conf = new SparkConf().setMaster("local[2]").setAppName(this.getClass.toString)
+    val conf = new SparkConf().setAppName(this.getClass.toString)
     val ssc = new StreamingContext(conf, Seconds(1))
-    val hdfsPath = "hdfs:///user/hive/warehouse/income/"
+    val hdfsPath = "/user/hive/warehouse/income"
     val incomeCsv = ssc.textFileStream(hdfsPath)
     // Format of the data is
     //GEO.id,GEO.id2,GEO.display-label,VD01
     //Id,Id2,Geography,Median family income in 1999
     //8600000US998HH,998HH,"998HH 5-Digit ZCTA, 998 3-Digit ZCTA",0
-    val parsedCsv = parse(incomeCsv)
-    // 2nd element (index 1) is zip code, last element (index 3) is income
-    // We take the first 3 digits of zip code and find average income in that geographic area
-    val areaIncomeStream = parsedCsv.map(record => (record(1).substring(0, 3), record(3).toInt))
+    val areaIncomeStream = parse(incomeCsv)
 
     // First element of the tuple in DStream are total incomes, second is total number of zip codes
     // in that geographic area, for which the income is shown
@@ -39,11 +36,11 @@ object DirectKafkaAverageHouseholdIncome {
 
   }
 
-  def parse(incomeCsv: DStream[String]): DStream[List[String]] = {
+  def parse(incomeCsv: DStream[String]): DStream[(String, Int)] = {
     println("here1")
     val builder = StringBuilder.newBuilder
     println("here2")
-    incomeCsv.map(x => {
+    val parsedCsv = incomeCsv.map(x => {
       println("here3")
       var result = List[String]()
       var withinQuotes = false
@@ -62,5 +59,9 @@ object DirectKafkaAverageHouseholdIncome {
       println ("REsult just added:" + builder.toString)
       result :+ builder.toString
     })
+    // 2nd element (index 1) is zip code, last element (index 3) is income
+    // We take the first 3 digits of zip code and find average income in that geographic area
+    parsedCsv.map(record => (record(1).substring(0, 3), record(3).toInt))
+
   }
 }
