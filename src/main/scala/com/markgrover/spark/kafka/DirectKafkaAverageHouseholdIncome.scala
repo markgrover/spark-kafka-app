34edef586509d040502976804fc8ce8a295b8481
Mark Grover <mark@apache.org>
2016-01-12 14:05:58 -0800

    Correcting the name of the host by adding the port

--- src/main/scala/com/markgrover/spark/kafka/DirectKafkaAverageHouseholdIncome.scala
+++ src/main/scala/com/markgrover/spark/kafka/DirectKafkaAverageHouseholdIncome.scala
@@ -12,7 +12,7 @@ object DirectKafkaAverageHouseholdIncome {
     val ssc = new StreamingContext(conf, Seconds(1))
     val hdfsPath = "/user/hive/warehouse/income"
     val kafkaParams: Map[String, String] = Map("auto.offset.reset" -> "earliest",
-      "bootstrap.servers" -> "mgrover-st-1.vpc.cloudera.com",
+      "bootstrap.servers" -> "mgrover-st-1.vpc.cloudera.com:9092",
       "key.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer",
       "value.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer")
 
